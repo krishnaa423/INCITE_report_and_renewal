4627000c7804bce00d5510f06774697c48c2dd91
krishnaa423 <srikrishnaa526@gmail.com>
2026-06-23 17:08:26 -0400

    Added gitignore

--- run_large.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-#SBATCH --account=mat280
-#SBATCH --job-name=struct_job
-#SBATCH --partition=batch
-#SBATCH --nodes=150
-#SBATCH --time=10:00:00
-
-srun -n 1200 --gpus-per-task=1 python ./run_large.py &> ./run_large.py.out
--- run_small.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-#SBATCH --account=mat280
-#SBATCH --job-name=struct_job
-#SBATCH --partition=batch
-#SBATCH --nodes=1
-#SBATCH --time=00:10:00
-
-srun -n 8 --gpus-per-task=1 python ./run_small.py &> ./run_small.py.out
